3197d01092d9e0268bd5a0d303c334b961d97f21
Mücahit Nezir <mucahitnezir@gmail.com>
2021-08-01 00:40:03 +0300

    Inherit CurrentUserDto from DtoBase

--- VideoApp.Entities/DTOs/CurrentUserDto.cs
+++ VideoApp.Entities/DTOs/CurrentUserDto.cs
@@ -3,12 +3,10 @@ using VideoApp.Core.Entities;
 
 namespace VideoApp.Entities.DTOs
 {
-    public class CurrentUserDto : IDto
+    public class CurrentUserDto : DtoBase
     {
-        public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public DateTime CreatedAt { get; set; }
     }
 }
